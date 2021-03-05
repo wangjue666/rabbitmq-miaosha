b9a164fd587e87e9442ac263308b65b8e2edcba3
王珏 <wangjuesix@gmail.com>
2021-03-05 15:14:08 +0800

    定义秒杀Messsage struct

---
+++ datamodels/message.go
@@ -0,0 +1,13 @@
+package datamodels
+
+// 简单的消息体
+type Message struct {
+	ProductID int64
+	UserID    int64
+	Time      int64
+}
+
+// 创建结构体
+func NewMessage(userId int64, productId int64, timeUnix int64) *Message {
+	return &Message{UserID: userId, ProductID: productId, Time: timeUnix}
+}
